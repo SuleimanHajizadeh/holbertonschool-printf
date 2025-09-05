f5952d78dc64065cc6b055f0c02fb78f3950ebe2
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-05 08:42:24 +0400

    sdsd

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include "main.h"
-#include <stdio.h>int main(void)
+#include <stdio.h>
+
+// Prototype for _printf_custom
+int _printf_custom(const char *format, ...);
+
+int main(void)
 {
-        unsigned int i;        i = _printf_custom("Char: %c Str: %upper\n", 'c', "salam");
+        unsigned int i;
+        i = _printf_custom("Char: %c Str: %upper\n", 'c', "salam");
         printf("Count: %d\n", i);
         return (0);
 }
--- printf.c
+++ printf.c
@@ -10,13 +10,19 @@ void print_char(char c)
 }
 unsigned int print_string(char *str)
 {
-        unsigned int i;        for (i = 0; str[i] != '\0'; i++);        write(1, str, i);
+        unsigned int i;
+        for (i = 0; str[i] != '\0'; i++)
+                ;
+        write(1, str, i);
         return (i);
 }
 char *to_upper(char *str)
 {
         unsigned int i;
-        char *upper;        for (i = 0; str[i] != '\0'; i++);        upper = malloc((i + 1) * sizeof(char));
+        char *upper;
+        for (i = 0; str[i] != '\0'; i++)
+                ;
+        upper = malloc((i + 1) * sizeof(char));
         if (upper == NULL)
                 return (NULL);
         for (i = 0; str[i] != '\0'; i++)
@@ -29,29 +35,31 @@ int _printf_custom(const char *format, ...)
 {
         va_list args;
         unsigned int i, j;
-        char *str;        j = 0;
+        char *str;
+        j = 0;
         for (i = 0; format[i] != '\0'; i++)
         {
                 if (format[i] == '%')
                 {
-                        if (format[i+1] == 'l' && format[i+2] == 'o' && format[i+3] == 'w' && format[i+4] == 'e' && format[i+5] == 'r')
+                        if (format[i + 1] == 'l' && format[i + 2] == 'o' && format[i + 3] == 'w' && format[i + 4] == 'e' && format[i + 5] == 'r')
                                 j++;
-                        else if (format[i+1] == 'i' && format[i+2] == 's' && format[i+3] == 'n' && format[i+4] == 'u' && format[i+5] == 'm' && format[i+6] == 'b' && format[i+7] == 'e' && format[i+8] == 'r')
+                        else if (format[i + 1] == 'i' && format[i + 2] == 's' && format[i + 3] == 'n' && format[i + 4] == 'u' && format[i + 5] == 'm' && format[i + 6] == 'b' && format[i + 7] == 'e' && format[i + 8] == 'r')
                                 j++;
-                        else if (format[i+1] == 'i' && format[i+2] == 's' && format[i+3] == 'a' && format[i+4] == 'l' && format[i+5] == 'p' && format[i+6] == 'h' && format[i+7] == 'a')
+                        else if (format[i + 1] == 'i' && format[i + 2] == 's' && format[i + 3] == 'a' && format[i + 4] == 'l' && format[i + 5] == 'p' && format[i + 6] == 'h' && format[i + 7] == 'a')
                                 j++;
                 }
-                }
-        va_start(args, format);        j = 0;
+        }
+        va_start(args, format);
+        j = 0;
         for (i = 0; format[i] != '\0'; i++)
         {
                 if (format[i] != '%')
                         print_char(format[i]), j++;
-                else if (format[i+1] == 'c')
-                        print_char((char) va_arg(args, int)), i++, j++;
-                else if (format[i+1] == 's')
+                else if (format[i + 1] == 'c')
+                        print_char((char)va_arg(args, int)), i++, j++;
+                else if (format[i + 1] == 's')
                         j += print_string(va_arg(args, char *)), i++;
-                else if (format[i+1] == 'u' && format[i+2] == 'p' && format[i+3] == 'p' && format[i+4] == 'e' && format[i+5] == 'r')
+                else if (format[i + 1] == 'u' && format[i + 2] == 'p' && format[i + 3] == 'p' && format[i + 4] == 'e' && format[i + 5] == 'r')
                 {
                         str = to_upper(va_arg(args, char *));
                         if (str == NULL)
@@ -59,6 +67,7 @@ int _printf_custom(const char *format, ...)
                         j += print_string(str), i += 5;
                         free(str);
                 }
-        }        va_end(args);
+        }
+        va_end(args);
         return (j);
 }
