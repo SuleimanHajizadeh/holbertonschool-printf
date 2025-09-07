0ec1425db6accfe83e77de59c59942026e32d9df
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-07 19:07:24 +0000

    sdsd

--- _printf.c
+++ _printf.c
@@ -1,19 +1,30 @@
-for (i = 0; format[i] != '\0'; i++)
-{
-    if (format[i] == '%')
-    {
-        i++; /* move to the specifier */
+#include "main.h"
+#include <stdarg.h>
 
-        if (format[i] == 'p')
-            buf_index = print_pointer(va_arg(args, void *), buffer, &buf_index, &count);
-        else if (format[i] == 's')
-            buf_index = print_string(va_arg(args, char *), buffer, &buf_index, &count);
-        /* ... other specifiers ... */
-        else
-            buf_index = print_char(format[i], buffer, &buf_index, &count);
-    }
-    else
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int i, count = 0, buf_index = 0;
+    char buffer[1024];
+
+    va_start(args, format);
+
+    for (i = 0; format[i] != '\0'; i++)
     {
-        buf_index = print_char(format[i], buffer, &buf_index, &count);
+        if (format[i] == '%')
+        {
+            i++; /* move to specifier */
+            if (format[i] == 'p')
+                buf_index = print_pointer(va_arg(args, void *), buffer, &buf_index, &count);
+            else
+                buf_index = print_char(format[i], buffer, &buf_index, &count);
+        }
+        else
+        {
+            buf_index = print_char(format[i], buffer, &buf_index, &count);
+        }
     }
+
+    va_end(args);
+    return (count);
 }
