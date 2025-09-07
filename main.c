0d5bcc6cbca3aaf19b07dc2dd0eca9eec0db4aed
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-07 10:47:18 +0000

    sdsd

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-    _printf("Char: [%c]\n", 'A');
-    _printf("String: [%s]\n", "Hello World!");
-    _printf("Percent: [%%]\n");
-
+    _printf("%b\n", 98);
     return (0);
 }
