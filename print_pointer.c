25f2bb22d45594b5a212bd9e8d2a6644cf181abc
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-07 19:30:27 +0000

    sdsd

--- print_pointer.c
+++ print_pointer.c
@@ -2,15 +2,24 @@
 
 int print_pointer(void *ptr, char *buffer, int *buf_index, int *count)
 {
-    unsigned long addr = (unsigned long)ptr;
+    unsigned long addr;
+
+    if (ptr == NULL)
+    {
+        *buf_index = print_char('(', buffer, buf_index, count);
+        *buf_index = print_char('n', buffer, buf_index, count);
+        *buf_index = print_char('i', buffer, buf_index, count);
+        *buf_index = print_char('l', buffer, buf_index, count);
+        *buf_index = print_char(')', buffer, buf_index, count);
+        return (*buf_index);
+    }
+
+    addr = (unsigned long)ptr;
 
     *buf_index = print_char('0', buffer, buf_index, count);
     *buf_index = print_char('x', buffer, buf_index, count);
 
-    if (addr == 0)
-        *buf_index = print_char('0', buffer, buf_index, count);
-    else
-        *buf_index = print_unsigned_long(addr, 16, 0, buffer, buf_index, count);
+    *buf_index = print_unsigned_long(addr, 16, 0, buffer, buf_index, count);
 
     return (*buf_index);
 }
