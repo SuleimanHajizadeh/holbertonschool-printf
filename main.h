391736f93ee463fd1804d0977c74df28e91a9239
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-07 18:07:41 +0000

    sdsds

--- main.h
+++ main.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 
 int _printf(const char *format, ...);
-int _putchar(char c);
-int print_char(char c);
-int print_string(char *s);
-int print_number(int n);
-int print_binary(unsigned int n);
+
+int print_char(char c, char *buffer, int *buf_index, int *count);
+int print_string(char *s, char *buffer, int *buf_index, int *count);
+int print_number(int n, char *buffer, int *buf_index, int *count);
+int print_binary(unsigned int n, char *buffer, int *buf_index, int *count);
 int print_unsigned(unsigned int n, int base, int uppercase,
                    char *buffer, int *buf_index, int *count);
 
-#endif /* MAIN_H */
+#endif
