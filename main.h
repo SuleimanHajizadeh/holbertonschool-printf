36d4c91e8925a7a90a8a0a6f53c0f92b87c28833
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-09-07 19:54:42 +0000

    sdsds

--- main.h
+++ main.h
@@ -1,30 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdarg.h>
-#include <unistd.h>
-#include <stddef.h>
-
-/* _printf function */
 int _printf(const char *format, ...);
-
-/* Printing functions */
 int print_char(char c, char *buffer, int *buf_index, int *count);
 int print_string(char *s, char *buffer, int *buf_index, int *count);
 int print_number(int n, char *buffer, int *buf_index, int *count);
 int print_unsigned(unsigned int n, int base, int uppercase,
                    char *buffer, int *buf_index, int *count);
-int print_binary(unsigned int n, char *buffer, int *buf_index, int *count);
-int print_pointer(void *ptr, char *buffer, int *buf_index, int *count);
+int print_pointer(void *p, char *buffer, int *buf_index, int *count);
 
-/* Flags structure */
-typedef struct flags
-{
-    int plus;
-    int space;
-    int hash;
-} flags_t;
-
-flags_t get_flags(const char *format, int *i);
-
-#endif /* MAIN_H */
+#endif
