a57200f0fbddb1c9e3d7c00a29ff9688bbfb0abe
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2025-09-12 13:17:08 +0400

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -1,26 +1,34 @@
 #include "main.h"
 
-flags_t get_flags(const char *format, int *i)
+/**
+ * get_flags - Calculates active flags
+ * @format: Formatted string in which to print the arguments
+ * @i: take a parameter.
+ * Return: Flags:
+ */
+int get_flags(const char *format, int *i)
 {
-    flags_t f;
-    int j;
+	/* - + 0 # ' ' */
+	/* 1 2 4 8  16 */
+	int j, curr_i;
+	int flags = 0;
+	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
+	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-    f.plus = 0;
-    f.space = 0;
-    f.hash = 0;
-    j = *i;
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
+	{
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
+			{
+				flags |= FLAGS_ARR[j];
+				break;
+			}
 
-    while (format[j] == '+' || format[j] == ' ' || format[j] == '#')
-    {
-        if (format[j] == '+')
-            f.plus = 1;
-        if (format[j] == ' ')
-            f.space = 1;
-        if (format[j] == '#')
-            f.hash = 1;
-        j++;
-    }
+		if (FLAGS_CH[j] == 0)
+			break;
+	}
 
-    *i = j - 1;
-    return f;
+	*i = curr_i - 1;
+
+	return (flags);
 }
