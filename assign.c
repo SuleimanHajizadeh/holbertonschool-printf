78c692dbf9c512709b0684520a24fbd5093538aa
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2025-09-21 11:16:38 +0400

    Update assign.c

--- assign.c
+++ assign.c
@@ -1,43 +1,17 @@
 #include "main.h"
 
-/**
- * _assign - assing and count function
- * @buff: str var
- * @count: index counter
- * @value: value in the index
- * Return: int
- */
-
 int _assign(char *buff, int count, char *value)
 {
-	int i = 0;
-
-	while (value[i])
-	{
-		buff[i] = value[i];
-		count++;
-		i++;
-	}
-	return (count);
+    int i = 0;
+    while (value[i])
+        buff[i++] = value[i++];
+    return count + _strlen(value);
 }
-
-/**
- * _rev_assign - reversed assing function and return count
- * @buff: str var
- * @count: index counter
- * @value: value in the index
- * Return: int
- */
 
 int _rev_assign(char *buff, int count, char *value)
 {
-	int len = _strlen(value);
-	int i;
-
-	for (i = 0; i <= len; i++)
-	{
-		buff[i] = value[len - i];
-		count++;
-	}
-	return (count);
+    int len = _strlen(value), i;
+    for (i = 0; i < len; i++)
+        buff[i] = value[len - i - 1];
+    return count + len;
 }
