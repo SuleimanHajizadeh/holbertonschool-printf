752a6a1b7b4bcd5656bf7d6485281104de8a7276
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2025-09-21 11:15:12 +0400

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,51 +1,55 @@
 #include "main.h"
-
-/**
- * _printf - print function
- * @format: str
- * Return: integer
- */
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int _printf(const char *format, ...)
 {
-	char *buff;
-	int  i = 0, count = 0;
-	va_list arg_value;
-	int (*func)(char *, int, va_list);
+    char *buff;
+    int i = 0, count = 0;
+    va_list arg_value;
+    int flags;
 
-	buff = malloc(4000);
-	if ((!format || !buff) || (format[0] == '%' && format[1] == '\0'))
-	{
-		free(buff);
-		exit(1);
-	}
+    buff = malloc(4000);
+    if (!format || !buff || (format[0] == '%' && format[1] == '\0'))
+    {
+        free(buff);
+        exit(1);
+    }
 
-	va_start(arg_value, format);
+    va_start(arg_value, format);
 
-	while (format[i])
-	{
-		if (format[i] != '%')
-		{
-			buff[count] = format[i];
-			count++;
-		}
-		else
-		{
-			func = check_prtr(format[i + 1]);
-			if (!func)
-			{
-				buff[count] = '%';
-				i++;
-				count++;
-				continue;
-			}
-			count = func(&buff[count], count, arg_value);
-			i++;
-		}
-		i++;
-	}
-	write(1, buff, count);
-	va_end(arg_value);
-	free(buff);
-	return (count);
+    while (format[i])
+    {
+        if (format[i] != '%')
+        {
+            buff[count++] = format[i];
+        }
+        else
+        {
+            i++;
+            flags = 0;
+            if (format[i] == '+')
+            {
+                flags |= FLAG_PLUS;
+                i++;
+            }
+
+            int (*func)(char *, int, va_list, int) = check_prtr(format[i]);
+            if (!func)
+            {
+                buff[count++] = '%';
+                buff[count++] = format[i];
+            }
+            else
+            {
+                count = func(&buff[count], 0, arg_value, flags);
+            }
+        }
+        i++;
+    }
+    write(1, buff, count);
+    va_end(arg_value);
+    free(buff);
+    return count;
 }
