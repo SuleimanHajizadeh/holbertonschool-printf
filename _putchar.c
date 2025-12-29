488c863ae93d52c48f7fcd033f172a2c0d58af97
Suleiman Hajizadeh <suleyman.hacizade1@gmail.com>
2025-12-30 01:32:27 +0400

    sd

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes a character to stdout
+ * @c: character
+ *
+ * Return: 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
