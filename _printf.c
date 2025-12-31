4df2149ea42a91e651763046f3072587fe125259
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-31 15:27:25 +0000

    sd

--- _printf.c
+++ _printf.c
@@ -1,6 +1,62 @@
 #include "main.h"
 
 /**
+ * _putchar - Writes a single character to stdout
+ * @c: Character to print
+ * Return: 1
+ */
+int _putchar(char c)
+{
+    return (write(1, &c, 1));
+}
+
+/**
+ * print_string - Writes a string to stdout
+ * @s: String to print
+ * Return: Number of characters printed
+ */
+int print_string(char *s)
+{
+    int len = 0;
+
+    if (!s)
+        s = "(null)";
+
+    while (*s)
+    {
+        _putchar(*s);
+        s++;
+        len++;
+    }
+    return (len);
+}
+
+/**
+ * print_number - Prints an integer to stdout
+ * @n: Integer to print
+ * Return: Number of characters printed
+ */
+int print_number(int n)
+{
+    int len = 0;
+    unsigned int num;
+
+    if (n < 0)
+    {
+        len += _putchar('-');
+        num = -n;
+    }
+    else
+        num = n;
+
+    if (num / 10)
+        len += print_number(num / 10);
+
+    len += _putchar((num % 10) + '0');
+    return (len);
+}
+
+/**
  * handle_format - Handles format specifiers
  * @format: Format specifier
  * @args: List of arguments
@@ -9,29 +65,29 @@
  */
 int handle_format(const char format, va_list args)
 {
-	int count = 0;
+    int count = 0;
 
-	switch (format)
-	{
-	case 'c':
-		count += _putchar(va_arg(args, int));
-		break;
-	case 's':
-		count += print_string(va_arg(args, char *));
-		break;
-	case '%':
-		count += _putchar('%');
-		break;
-	case 'd':
-	case 'i':
-		count += print_number(va_arg(args, int));
-		break;
-	default:
-		count += _putchar('%');
-		count += _putchar(format);
-		break;
-	}
-	return (count);
+    switch (format)
+    {
+    case 'c':
+        count += _putchar(va_arg(args, int));
+        break;
+    case 's':
+        count += print_string(va_arg(args, char *));
+        break;
+    case '%':
+        count += _putchar('%');
+        break;
+    case 'd':
+    case 'i':
+        count += print_number(va_arg(args, int));
+        break;
+    default:
+        count += _putchar('%');
+        count += _putchar(format);
+        break;
+    }
+    return (count);
 }
 
 /**
@@ -42,34 +98,31 @@ int handle_format(const char format, va_list args)
  */
 int _printf(const char *format, ...)
 {
-	int count = 0;
-	va_list args;
+    int count = 0;
+    va_list args;
 
-	if (!format)
-		return (-1);
+    if (!format)
+        return (-1);
 
-	va_start(args, format);
+    va_start(args, format);
 
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			if (!*format)
-			{
-				va_end(args);
-				return (-1);
-			}
-			count += handle_format(*format, args);
-		}
-		else
-		{
-			count += _putchar(*format);
-		}
-		format++;
-	}
-	va_end(args);
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+            if (!*format)  /* string sonunda % varsa, heç nə çap et */
+                break;
+            count += handle_format(*format, args);
+        }
+        else
+        {
+            count += _putchar(*format);
+        }
+        format++;
+    }
 
-	return (count);
+    va_end(args);
+    return (count);
 }
 
