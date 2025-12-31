c1974d0de05043fe5d2b88420e936d37586b1dab
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-31 15:20:57 +0000

    sd

--- _printf.c
+++ _printf.c
@@ -1,78 +1,51 @@
 #include "main.h"
 
 /**
- * print_char - Writes a single character to stdout
- * @c: Character to print
- * Return: 1
- */
-int print_char(char c)
-{
-    return (write(1, &c, 1));
-}
-
-/**
- * print_string - Writes a string to stdout
- * @s: String to print
- * Return: Number of characters printed
- */
-int print_string(char *s)
-{
-    int len = 0;
-
-    if (!s)
-        s = "(null)";
-
-    while (*s)
-    {
-        write(1, s, 1);
-        s++;
-        len++;
-    }
-    return (len);
-}
-
-/**
- * _printf - Custom printf function (task 0)
- * @format: Format string
- * Return: Number of characters printed
+ * _printf - function that produces output according to a format.
+ * @format: Is an string with the sentences or plain text to print.
+ *
+ * Return: The lenght of all sentences and plain text.
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, len = 0;
+	int i, (*f_print)(va_list), length_full = 0;
+	va_list arg_list;
 
-    if (!format)
-        return (-1);
-
-    va_start(args, format);
-
-    while (format[i])
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (!format[i])  /* string sonunda % varsa, heç nə çap et */
-                break;
-
-            if (format[i] == 'c')
-                len += print_char(va_arg(args, int));
-            else if (format[i] == 's')
-                len += print_string(va_arg(args, char *));
-            else if (format[i] == '%')
-                len += print_char('%');
-            else
-            {
-                /* Unknown specifier: print as-is */
-                len += print_char('%');
-                len += print_char(format[i]);
-            }
-        }
-        else
-            len += print_char(format[i]);
-        i++;
-    }
-
-    va_end(args);
-    return (len);
+	if (format)
+	{
+		va_start(arg_list, format);
+		for (i = 0; format[i]; i++)
+		{
+			if (format[i] == '%' && format[i + 1])
+			{
+				if (format[i + 1] == '%')
+				{
+					_putchar('%');
+					i++;
+					length_full++;
+					continue;
+				}
+				f_print = RCL(format[i + 1]);
+				if (f_print == NULL && format[i + 1])
+				{
+					_putchar(format[i]);
+					_putchar(format[i + 1]);
+					length_full += 2;
+					i++;
+					continue;
+				}
+				length_full += f_print(arg_list);
+				i++;
+				continue;
+			}
+			else if (format[i] == '%')
+				return (-1);
+			_putchar(format[i]);
+			length_full++;
+		}
+	}
+	else
+		return (-1);
+	return (length_full);
 }
 
