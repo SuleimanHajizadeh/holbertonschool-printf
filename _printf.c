cba82fbdc702a0f4e5c2fe53011cd61662756b6b
SuleimanHajizadeh <suleyman.hacizade1@gmail.com>
2025-12-31 15:44:57 +0000

    sd

--- _printf.c
+++ _printf.c
@@ -1,99 +1,98 @@
 #include "main.h"
 
 /**
- * _putchar - Writes a single character to stdout
- * @c: Character to print
+ * _putchar - writes a character to stdout
+ * @c: character to print
+ *
  * Return: 1
  */
 int _putchar(char c)
 {
-    return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 
 /**
- * print_string - Writes a string to stdout
- * @s: String to print
- * Return: Number of characters printed
+ * print_string - prints a string
+ * @s: string to print
+ *
+ * Return: number of characters printed
  */
 int print_string(char *s)
 {
-    int len = 0;
+	int len = 0;
 
-    if (!s)
-        s = "(null)";
+	if (!s)
+		s = "(null)";
 
-    while (*s)
-    {
-        _putchar(*s);
-        s++;
-        len++;
-    }
-    return (len);
+	while (s[len])
+	{
+		_putchar(s[len]);
+		len++;
+	}
+	return (len);
 }
 
 /**
- * handle_format - Handles format specifiers
- * @format: Format specifier
- * @args: List of arguments
+ * handle_format - handles format specifiers
+ * @format: format character
+ * @args: argument list
  *
- * Return: Number of characters printed
+ * Return: number of characters printed
  */
 int handle_format(const char format, va_list args)
 {
-    int count = 0;
+	int count = 0;
 
-    switch (format)
-    {
-    case 'c':
-        count += _putchar(va_arg(args, int));
-        break;
-    case 's':
-        count += print_string(va_arg(args, char *));
-        break;
-    case '%':
-        count += _putchar('%');
-        break;
-    default:
-        count += _putchar('%');
-        count += _putchar(format);
-        break;
-    }
-    return (count);
+	if (format == 'c')
+		count += _putchar(va_arg(args, int));
+	else if (format == 's')
+		count += print_string(va_arg(args, char *));
+	else if (format == '%')
+		count += _putchar('%');
+	else
+	{
+		count += _putchar('%');
+		count += _putchar(format);
+	}
+	return (count);
 }
 
 /**
- * _printf - Custom printf function (task 0)
- * @format: Format string
+ * _printf - custom printf function (Task 0)
+ * @format: format string
  *
- * Return: Number of characters printed
+ * Return: number of characters printed, or -1 on error
  */
 int _printf(const char *format, ...)
 {
-    int count = 0;
-    va_list args;
+	int count = 0;
+	va_list args;
 
-    if (!format)
-        return (-1);
+	if (!format)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            if (!*format)  /* String sonunda % varsa, heç nə çap etmir */
-                break;
-            count += handle_format(*format, args);
-        }
-        else
-        {
-            count += _putchar(*format);
-        }
-        format++;
-    }
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (!*format)
+			{
+				va_end(args);
+				return (-1);
+			}
+			count += handle_format(*format, args);
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
 
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
 
--- main.h
+++ main.h
@@ -4,14 +4,37 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* _printf function */
+/**
+ * _printf - custom printf function
+ * @format: format string
+ *
+ * Return: number of characters printed
+ */
 int _printf(const char *format, ...);
 
-/* Format handler */
+/**
+ * handle_format - handles format specifiers
+ * @format: format character
+ * @args: argument list
+ *
+ * Return: number of characters printed
+ */
 int handle_format(const char format, va_list args);
 
-/* Output functions */
+/**
+ * _putchar - writes a character to stdout
+ * @c: character to print
+ *
+ * Return: 1
+ */
 int _putchar(char c);
+
+/**
+ * print_string - prints a string
+ * @s: string to print
+ *
+ * Return: number of characters printed
+ */
 int print_string(char *s);
 
 #endif /* MAIN_H */
